c754e4c8b531cf8f8a489f10a0dc4dac48c99a0b
Mojtaba Afraz <65365973+mojtaba-afraz@users.noreply.github.com>
2022-07-17 17:50:38 +0430

    add Doxor Class

---
+++ src/index.js
@@ -0,0 +1,17 @@
+export default class Doxor {
+    constructor(name) {
+        this.name = name
+    }
+    #DatabaseBridge(callback, type = 'store') {
+        let request = indexedDB.open(this.name, 1);
+        request.onupgradeneeded = event => {
+            if (type === 'store') callback(request.result)
+        }
+        request.onsuccess = event => {
+            if (type !== 'store') {
+                callback(request.result)
+            }
+        }
+    }
+
+}
